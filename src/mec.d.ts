446d3cdf56d00de9212497193b13f8f6ffe47352
Kai Lawrence <mail@kailawrence.de>
2021-04-11 11:20:18 +0200

    Update mec2 typings

--- src/mec.d.ts
+++ src/mec.d.ts
@@ -1,10 +1,22 @@
+import { g2 } from 'g2-module';
 
-export const mec: mec2;
+export const mec: IMec2;
+
+export interface IMecProperty {
+  id: string,
+  init: (model: IMec2) => void,
+  remove: () => void,
+}
 
 // drawing context for g2
 type ctx = any;
 
-interface constraint {
+export interface IView extends IMecProperty {
+  id: string,
+  as: string,
+}
+
+export interface IConstraint extends IMecProperty {
   id: string,
   p1: string,
   p2: string,
@@ -14,16 +26,23 @@ interface constraint {
   },
 }
 
-interface node {
+export interface INode extends IMecProperty {
   base: boolean,
   x: number,
   y: number,
 }
 
-export interface mec2 {
-  init(): () => void,
-  tick(): () => void,
-  draw(ctx): () => void,
-  nodes: node[],
-  constraints: constraint[],
+export interface IMecPlugIns {
+  "nodes": INode[],
+  "views": IView[],
+  "constraints": IConstraint[]
+}
+
+export interface IMec2 extends IMecPlugIns {
+  id: string,
+  plugIns: any,
+  draw: (g2: g2) => void,
+  add: (list: keyof IMecPlugIns, o: any) => any,
+  preview: () => void,
+  pose: () => void,
 }
