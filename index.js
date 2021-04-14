f796db58669c6284cd10db3bff476fb330a9fd59
Kai Lawrence <mail@kailawrence.de>
2021-04-14 18:12:08 +0200

    Update typings

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-export { mec } from './src/mec.core'
+export { mec } from './src/mec.core';
 
-import './src/mec.node'
-import './src/mec.constraint'
-import './src/mec.drive'
-import './src/mec.model'
+import './src/mec.node';
+import './src/mec.constraint';
+import './src/mec.drive';
+import './src/mec.model';
 // import './src/mec.shape'
-import './src/mec.view'
+import './src/mec.view';
